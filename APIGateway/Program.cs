413c7e05e5307d547a4d6de44cd04136fe6de91d
cpshahan <shahan@etsu.edu>
2024-04-02 19:30:56 -0400

    Added HttpClient to web application so it can make Http requests without errors

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register HttpClient
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
