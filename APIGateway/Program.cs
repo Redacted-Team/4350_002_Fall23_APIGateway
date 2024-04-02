96681ab99b6eb8c36aee20ee6d5d7b567e2ef5e3
Kurt Brewer <BREWERKM1@ETSU.EDU>
2024-04-02 13:29:22 -0400

    port numbmer fixed

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,3 +1,6 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,5 +25,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+await app.UseOcelot();
+
 
 app.Run();
